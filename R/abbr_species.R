7396fb224ff97ce9cc8013030c16235ae822c4f0
brunomioto <brunomioto97@gmail.com>
2022-12-05 08:45:08 -0300

    abbr_species working and add pipe

--- R/abbr_species.R
+++ R/abbr_species.R
@@ -1,23 +1,30 @@
 #' Abbreviate species
 #'
 #' @param data Species dataset
-#' @param species_var Species variable name
+#' @param species_var Current species variable name. Defalts to "species"
 #'
 #' @export
 
-abbr_species <- function(data, species_var = "especie"){
+abbr_species <- function(data, species_var = "species"){
 
-  dados2 <- data %>%
-    dplyr::mutate(species_draft = dplyr::case_when(stringr::str_detect(species_var, "sp.") == TRUE ~ species_var,
-                                                   stringr::str_detect(species_var, "sp.") == FALSE ~
-                                                     stringr::str_replace(species_var, "\\w+", paste0(stringr::str_extract(species_var, "\\w"),".")))) %>%
+
+  data2 <- data %>%
+    dplyr::rename(species = .data[[species_var]]) %>%
+    dplyr::mutate(species_draft = dplyr::case_when(stringr::str_detect(species, "sp\\.") == TRUE ~ species,
+                                                   stringr::str_detect(species, "sp\\.") == FALSE ~
+                                                     stringr::str_replace(species,
+                                                                          "\\w+",
+                                                                          paste0(stringr::str_extract(species, "\\w"),".")))) %>%
     dplyr::add_count(species_draft, name = "rep") %>%
-    dplyr::mutate(species_var = ifelse(rep > 1,
-                                   stringr::str_replace(species_var, "\\w+", paste0(stringr::str_extract(species_var, "\\w{3}"),".")),
-                                   species_draft)) %>%
+    dplyr::mutate(species_draft = ifelse(rep > 1,
+                                   stringr::str_replace(species,
+                                                        "\\w+",
+                                                        paste0(stringr::str_extract(species, "\\w{3}"),".")),
+                                   species_draft),
+                  species = species_draft) %>%
     dplyr::select(-c(rep,species_draft)) %>%
     dplyr::as_tibble()
 
-  return(dados2)
+  return(data2)
 
 }

44f0a177dbbf7ef84673d8f20fdc1b6a31666026
brunomioto <brunomioto97@gmail.com>
2022-12-16 10:07:50 -0300

    update format_table

--- R/format_table.R
+++ R/format_table.R
@@ -2,10 +2,14 @@
 #'
 #' @param data Species dataset
 #' @param species_var The species variable name. Defaults to "especie"
+#' @param measure_var The measures variable name. Defaults to "medidas"
 #' @param sep Separator of measures. Defaults to " - "
 #'
 #' @export
-format_table <- function(data, species_var = "especie", sep = " - ") {
+format_table <- function(data,
+                         species_var = "especie",
+                         measure_var = "medidas",
+                         sep = " - ") {
 
 data2 <- data %>%
   tidyr::pivot_longer(
@@ -16,7 +20,8 @@ data2 <- data %>%
   tidyr::separate(col = "values", into = c("min", "max"), sep = sep) %>%
   dplyr::mutate(min = as.double(min),
                 max = as.double(max)) %>%
-  dplyr::rename(species_var = 1)
+  dplyr::rename({{species_var}} := 1,
+                {{measure_var}} := "measure")
 
 return(data2)
 
